ac0974d9aed60055e36d7449f124bac6b3863bf3
alimaghamfar <140330761+alimaghamfar@users.noreply.github.com>
2023-07-24 14:42:38 +0330

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -1,22 +1,23 @@
-import streamlit as st
 import cv2
-cap = cv2.VideoCapture(0)
-st.title("in the name of god")
-st.header('test webcam')
-st.subheader('warping detection')
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-frame_placeholder=st.empty()
+faceCascade = cv2.CascadeClassifier(cv2.haarcascades+'haarcascade_frontalface_default.xml')
 
-stop_button_pressed=st.button("stop")
-while cap.isOpened() and not stop_button_pressed:
-    ret, frame = cap.read()
-    if not ret:
-        st.write("video ended")
-        break
-    #frame=model.predict(source=frame,show=False,conf=0.8)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_placeholder.image(frame,channels="RGB")
-    if cv2.waitKey(1) & 0xFF == ord('q') or stop_button_pressed:
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+class VideoTransformer(VideoTransformerBase):
+    def __init__(self):
+        self.i = 0
+
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+        i =self.i+1
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (95, 207, 30), 3)
+            cv2.rectangle(img, (x, y - 40), (x + w, y), (95, 207, 30), -1)
+            cv2.putText(img, 'F-' + str(i), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2)
+
+        return img
+
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
