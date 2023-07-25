48bcd5d8345dfbb826eeabe3c59c694e2396978d
alimaghamfar <140330761+alimaghamfar@users.noreply.github.com>
2023-07-25 14:08:41 +0330

    Update stream2.py

--- stream2.py
+++ stream2.py
@@ -1,34 +1,35 @@
 import threading
-
+import av
 import cv2
 import streamlit as st
 from matplotlib import pyplot as plt
 
-from streamlit_webrtc import webrtc_streamer
-
+from streamlit_webrtc import (
+    RTCConfiguration,
+    WebRtcMode,
+    WebRtcStreamerContext,
+    webrtc_streamer,
+)
 lock = threading.Lock()
 img_container = {"img": None}
 
 
-def video_frame_callback(frame):
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     img = frame.to_ndarray(format="bgr24")
-    with lock:
-        img_container["img"] = img
+    cv2.rectangle(img,(315,10),(380,80),(0,210,0),2)
 
-    return frame
+    #img=cv2.imread(frame)
+    #img=cv2.cvtColor(img,cv2.COLOR_BGR2BGRA)
+    #with lock:
+       # img_container["img"] = img
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-ctx = webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
-
-fig_place = st.empty()
-fig, ax = plt.subplots(1, 1)
-
-while ctx.state.playing:
-    with lock:
-        img = img_container["img"]
-    if img is None:
-        continue
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ax.cla()
-    ax.hist(gray.ravel(), 256, [0, 256])
-    fig_place.pyplot(fig)
+webrtc_streamer(
+            key="object-detection",
+            mode=WebRtcMode.SENDRECV,
+            #rtc_configuration=RTC_CONFIGURATION,
+            video_frame_callback=video_frame_callback,
+            media_stream_constraints={"video": True, "audio": False},
+            async_processing=True,
+        )
